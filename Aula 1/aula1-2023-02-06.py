977addcf425285e85507ff76cf5bdea93c2cd574
Vinícius Chagas <68537257+ViniiCh4g4s@users.noreply.github.com>
2023-02-06 22:39:22 -0300

    Add files via upload

---
+++ Aula 1/aula1-2023-02-06.py
@@ -0,0 +1,10 @@
+# Primeiro exemplo de manipulação de dados usando Python Pandas
+# Autor: Vinicius Chagas
+# Data: 6 de fevereiro de 2023
+
+# -------------------------------------------------------------------
+
+# importando biblioteca pandas no Python
+import pandas as pd
+
+combustiveis_df = pd.read_csv("")
